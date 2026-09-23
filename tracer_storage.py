bc58d18eb269d91ca58e408c075116b87c5a5825
chandanasree425 <chandanasree425@gmail.com>
2026-09-23 14:11:52 +0530

    Add SQLite storage for tracer states

--- tracer_storage.py
+++ tracer_storage.py
@@ -1,6 +1,24 @@
+import sqlite3
+import time
+
+
 class TracerStorage:
-    def __init__(self):
+    def __init__(self, database_name="pychronicle.db"):
         self.states = []
+
+        self.connection = sqlite3.connect(database_name)
+
+        self.connection.execute("""
+            CREATE TABLE IF NOT EXISTS execution_state (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                timestamp REAL,
+                line_number INTEGER,
+                variable_name TEXT,
+                serialized_value TEXT
+            )
+        """)
+
+        self.connection.commit()
 
     def add_state(self, line_number, variable_name, value):
         state = {
@@ -9,7 +27,19 @@ class TracerStorage:
             "value": value
         }
 
+       
         self.states.append(state)
+
+        self.connection.execute(
+        """
+        INSERT INTO execution_state
+        (timestamp, line_number, variable_name, serialized_value)
+        VALUES (?, ?, ?, ?)
+        """,
+        (time.time(), line_number, variable_name, str(value))
+    )
+
+        self.connection.commit()
 
     def get_states(self):
         return self.states
